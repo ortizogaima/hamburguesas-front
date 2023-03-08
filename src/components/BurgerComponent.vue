4248e5eb0891d48ae8516ca18004e4b3b6c0bb3d
ortizogaima <ortizogaimanol.labase@gmail.com>
2023-03-08 14:38:10 -0300

    Burger details in a modal

---
+++ src/components/BurgerComponent.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="burger-component">
+    <transition name="modal"> 
+      <div v-if="isOpen">
+        <div class="overlay" @click.self="isOpen = false;">
+          <div class="modal">
+            <h1>Hamburguesa: {{burger.nombre}}</h1>
+            <h2>Ingredientes:</h2>
+            <h3 v-for="ingrediente in burger.ingredientes" v-bind:key="(ingrediente)">{{ingrediente}}</h3>
+            <h2>Calorias: {{burger.calorias}}</h2>
+            <button @click="isOpen=false">Salir</button>
+          </div>
+        </div>
+      </div>
+    </transition>  
+    <button @click="isOpen=!isOpen">{{burger.nombre}}</button>
+  </div>
+
+</template>
+
+
+<script>
+
+export default {
+  name: 'BurgerComponent',
+  props: ['burger'],
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+}
+</script>
+
+<style scoped>
+.modal {
+  width: 500px;
+  margin: 0px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px 3px;
+  transition: all 0.2s ease-in;
+  font-family: Helvetica, Arial, sans-serif;
+}
+.fadeIn-enter {
+  opacity: 0;
+}
+
+.fadeIn-leave-active {
+  opacity: 0;
+  transition: all 0.2s step-end;
+}
+
+.fadeIn-enter .modal,
+.fadeIn-leave-active.modal {
+  transform: scale(1.1);
+}
+
+button {
+  padding: 7px;
+  margin-top: 10px;
+  background-color: #256a4b;
+  border-radius: 10px;
+  box-shadow: 3px 3px 0px 0px #00000033;
+  border: none;
+  color: white;
+  font-size: 1.1rem;
+}
+
+button:active{
+  background-color: #42b983;
+}
+
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: #00000094;
+  z-index: 999;
+  transition: opacity 0.2s ease;
+}
+</style> -->
--- src/views/Burgers.vue
+++ src/views/Burgers.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
-    <h1>Completa aquí el desafío</h1>
+  <div class="burgers-viables">
+    <ul>
+      <li class="burger-links" v-for="burger in burgers" v-bind:key="(burger.id)">
+        <BurgerComponent  :burger="burger" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import BurgerComponent from '@/components/BurgerComponent.vue'
+
 export default {
   name: 'Burgers',
+  data() {
+    return {
+      burgers: 'No hay hamburguesas disponibles',
+    };
+  },
+  components: {
+    BurgerComponent
+  },
+  methods: {
+    getBurgers() {
+      this.$http.get('https://hamburguesas-back.elevadev.cl/burger/')
+        .then((response) => { this.burgers = response.data; },
+        err => console.log(err));
+    },
+  },
+  created() {
+    console.log('Api rest used');
+    this.getBurgers();
+  },
 }
 </script>
 
